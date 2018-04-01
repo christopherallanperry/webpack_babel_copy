ae110980dcf15c875cff4583cf22ffb505a10324
chrisofarabia <christopherallanperry@yahoo.co.uk>
2018-04-01 17:15:36 +0100

    Re-implemented 'CopyWebpackPlugin' and tested it successfully

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -23,5 +24,18 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+      [
+        {
+          from: "index.html",
+          to: "",
+          toType: "file",
+          context: "./src",
+        }
+      ],
+      { copyUnmodified: true }
+    )
+  ]
 };
